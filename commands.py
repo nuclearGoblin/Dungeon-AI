3d685966b3c09f02b4c270b4d3f528d5495146dd
Kitty Harris <45541156+nuclearGoblin@users.noreply.github.com>
2024-02-20 10:54:11 -0700

    Set up reqs and such for DBs

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 #Imports
 import discord, re
 import numpy as np
+import pandas as pd
 
 #Setup
 client = discord.Client(intents=discord.Intents.none())
@@ -15,7 +16,8 @@ tree = discord.app_commands.CommandTree(client)
 )
 async def help(interaction: discord.Interaction):
     message = """**help** - Prints this help menu.
-**roll** - Rolls a die, default 1d20, see command for options."""
+**roll** - Rolls a die, default 1d20, see command for options.
+**link** - Links a character sheet to your user for this server."""
     await interaction.response.send_message(message,ephemeral=True)
 
 #Basic die rolls.
@@ -24,9 +26,9 @@ async def help(interaction: discord.Interaction):
     description="Default: rolls 1d20. Rolls a number of dice with minimum, maximum, and modifier."
 )
 @discord.app_commands.describe(
-    dice="String representing the dice rolled, in format `XdY+Z`, `XdY-Z`, or `XdY`.",
+    dice="String representing the dice rolled, in format `XdY+Z`, `XdY-Z`, or `XdY`. (Default: 1d20)",
     goal="Value to meet or exceed.",
-    private="Hide roll from other users"
+    private="Hide roll from other users. (Default: False)"
     #mod="Modifier for the die roll.",
     #high="Maximum value of the dice rolled (for XdY, this is Y).",
     #low="Minimum value of the dice rolled (for normal dice, this is 1).",
@@ -89,3 +91,32 @@ async def roll(interaction: discord.Interaction, dice: str="", goal: int=None, p
     rollname = rollname+"**."
     await interaction.response.send_message(rollname,ephemeral=private)
 
+#Character sheet linking
+@tree.command(
+    name="link",
+    description="Links a character sheet to your user on this server."
+)
+@discord.app_commands.describe(
+    url="The URL of your character sheet.",
+    default="Set the character sheet as your default character sheet. (Default: True)",
+    overwrite="Overwrite all character sheet settings for this server, deleting previously linked character sheets. (Default: False)"
+    allguilds="Use this character sheet in all Discord servers I am in. (Default: False)"
+    name="Name for the character sheet. (Default: Whatever you've written on the character sheet.)"
+)
+async def link(interaction: discord.Interaction, url: str="", default: bool=True, overwrite: bool=False, allguilds: bool=False, name: str=None):
+    await interaction.response.send_message("Sorry, I'm a placeholder!",ephemeral=True)
+    pass
+#So this should use two databases:
+#1. Database containing user/guild/character sheet data.
+# - User ID, Guild ID, Character ID, Character Name, "default" status.
+#2. Database containing character data.
+# - Character ID, all relevant status, integrated with google sheets.
+
+#Other commands to write:
+#unlink (if no ID provided, unlink all data in current guild. If "all guilds" option provided, delete all user data.)
+#mergecharacters (merge all characters associated with User ID into )
+#show (show all linked character sheets, with names + IDs. Users can only see their own data unless they are server owners, in which case they can see all data for the guild.)
+#rename (rename a character sheet. if no arg, update the name based on the sheet.)
+#skillroll (roll the associated skill+modifiers, this is the main function we want)
+#requestroll (be able to request players click a button and roll a thing.)
+#configure (bot settings per server, maybe uses a third database, things like who can view sheets and request rolls.)
