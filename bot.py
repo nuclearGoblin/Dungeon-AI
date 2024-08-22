6955658e262e5fd69c0756e49a36bdd1dd33eb1f
Kitty Harris <45541156+nuclearGoblin@users.noreply.github.com>
2024-08-22 16:36:42 +0000

    fix bad merge in code as well.

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #Imports
 import os, discord, json
 import pandas as pd
@@ -59,62 +58,4 @@ async def on_ready():
 if SPHINX == "sphinx":
     print("Finished running in sphinx mode. Exiting!")
     exit(0)
-=======
-#Imports
-import os, discord, json
-import pandas as pd
-from decs import *
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-
-#Secrets
-TOKEN = os.getenv('DISCORD_TOKEN')
-testguild = discord.Object(id=os.getenv('TEST_SERVER_ID'))
-
-#Google setup
-SAMPLE_SPREADSHEET_ID = "1sRKjgEWwBr9cS_9KEvog6Y6_Ud3E0wI3URK56seRcmc"
-SAMPLE_RANGE_NAME = "Character Sheet!B2:B5"
-
-#Call the Google API to make sure it's working.
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,range=SAMPLE_RANGE_NAME).execute()
-values = result.get("values",[])
-if not values:
-    raise ValueError("No data found in spreadsheet "+SAMPLE_SPREADSHEET_ID)
-else:
-    print("DEBUG =================")
-    for row in values: print(row)
-
-#Check the database on spin-up -- don't want things to blip on if they're broken.
-try: #Check if the tables needed exist
-    users = pd.read_sql("SELECT * FROM users",connection,dtype=types)
-except pd.io.sql.DatabaseError: #If they don't,
-    users = pd.DataFrame(columns=userCols)
-    #user id [1], list of guild ids [n], list of default character identifiers [n], list of character ids [m], list of guilds with access to character [awk m]
-    users.to_sql(name='users',con=connection) #Create them.
-    print("`users` table was not present -- created!")
-if False: #Not actually sure we need chars db
-    try: #Check if the tables needed exist
-        chars = pd.read_sql("SELECT * FROM chars",connection)
-    except pd.io.sql.DatabaseError: #If they don't,
-        chars = pd.DataFrame(columns=charCols)
-        chars.to_sql(name='chars',con=connection)
-        print("`chars` table was not present -- created!")
-try: #Check if the tables needed exist
-    guilds = pd.read_sql("SELECT * FROM guilds",connection,dtype=types)
-except pd.io.sql.DatabaseError: #If they don't,
-    guilds = pd.DataFrame(columns=guildCols)
-    guilds.to_sql(name='guilds',con=connection)
-    print("`guilds` table was not present -- created!")
-
-#Now we can get commands imported
-from commands import * #load our commands without need for ref
-
-#Verify connection in server output
-@client.event
-async def on_ready():
-    tree.clear_commands(guild=testguild)
-    await tree.sync()
-    print(f'{client.user} has connected to Discord!')
-#Run 
->>>>>>> Stashed changes
 client.run(TOKEN)
--- decs.py
+++ decs.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #Imports
 import discord, json, os
 import sqlite3 as sql
@@ -60,57 +59,4 @@ def strtolist(string):
     #Strip excess whitespace and quotes
     string = [x.strip().replace("'","").replace('"',"") for x in string]
     if string == ['']: string = []
-=======
-#Imports
-import discord, json
-import sqlite3 as sql
-import numpy as np
-from dotenv import load_dotenv
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-#Load up environment envs
-load_dotenv()
-
-#Discord stuff
-client = discord.Client(intents=discord.Intents.none())
-tree = discord.app_commands.CommandTree(client)
-
-#sql stuff
-userCols = ["userID","charIDs","guildAssociations","readonly"]
-guildCols = ["userID","guildIDs","mainCharIDs"]
-charCols = ["charID","accessLevel"]
-connection = sql.connect("characters.db")
-types = {'userID': np.dtype('int64')}
-
-#Google stuff
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-creds = service_account.Credentials.from_service_account_info(json.load(open("service.json")))
-service = build("sheets","v4",credentials=creds)
-sheet = service.spreadsheets()
-
-#Microfunctions
-def readonlytest(token):
-    try:
-        testinput = {"values":[["a"]]}; testloc = "Character Sheet!S41"
-        sheet.values().update(spreadsheetId=token,range=testloc,valueInputOption="USER_ENTERED",body=testinput).execute()
-        gotback = sheet.values().get(spreadsheetId=token,range=testloc).execute().get("values",[])
-        if gotback != testinput["values"]:
-            raise ValueError("Tried to input "+str(testinput)+"but got back "+str(gotback)+".")
-        return False
-    except HttpError:
-        print("read only.")
-        return True
-    
-def strtolist(string):
-    if type(string) != str: #fallback case for non-string passed in.
-        return string
-    #Chop off the start/end brackets
-    if(string[0] == "[" and string[-1] == "]"): string = string[1:-1]
-    string = string.split(",") #Separate entries
-    #Strip excess whitespace and quotes
-    string = [x.strip().replace("'","").replace('"',"") for x in string]
-    if string == ['']: string = []
->>>>>>> Stashed changes
     return string
